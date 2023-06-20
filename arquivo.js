9816586359c778461c5f76dc8743fcd31938fd34
DelisG <delisemarciano@gmail.com>
2023-06-20 15:52:11 -0300

    :package: Adicionando Biblioteca Chalk e pasta arquivo.js

---
+++ arquivo.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Cadastro válido'));
+console.log(chalk.bgWhiteBright('Cadastro inválido'));
+console.log(chalk.magenta('muito bem rosinha'));
+console.log(chalk.rgb(15, 100, 204).inverse('Hello!'))
