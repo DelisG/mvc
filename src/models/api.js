a2f5bf6d8a2eeea83e16635fdec0f9fa592f0b8d
DelisG <delisemarciano@gmail.com>
2023-06-26 16:54:41 -0300

    :bricks: Construindo e testando modelo de Api

---
+++ src/models/api.js
@@ -0,0 +1,26 @@
+[
+  {
+    nome: "Delis Maria Da Silva",
+    email: "delisg@gmail.com",
+  },
+][
+  {
+    nome: "Mariana Maria Da Silva",
+    email: "marianag@gmail.com",
+  }
+][
+  {
+    nome: "Glaucy Maria Da Silva",
+    email: "glaucyg@gmail.com",
+  }
+][
+  {
+    nome: "Ingrid Maria Da Silva",
+    email: "ingridg@gmail.com",
+  }
+][
+  {
+    nome: "Renata Maria Da Silva",
+    email: "renatag@gmail.com",
+  }
+];
