7e27811346d922facc6de66ab103912a82e834c7
DelisG <delisemarciano@gmail.com>
2023-07-22 15:28:11 -0300

    :wrench: Alterações de revisão de código

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,9 +1,9 @@
+import mongoose from "mongoose";
 
-import mongoose from 'mongoose'
-
-mongoose.connect("mongodb+srv://admin:delis123@cluster0.q0ygpfy.mongodb.net/senac2");
+mongoose.connect(
+  "mongodb+srv://admin:delis123@cluster0.q0ygpfy.mongodb.net/senac2"
+);
 
 let db = mongoose.connection;
 
 export default db;
-
